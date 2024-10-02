0d445707654fe8149ac50bd75553031022baca40
Ibrahim9878 <talibliibrahim28@gmail.com>
2024-10-02 18:49:12 +0400

    Add project files.

---
+++ Client SS/Program.cs
@@ -0,0 +1,78 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Timers;
+
+
+var client = new UdpClient();
+var iPEndPoint = new IPEndPoint(IPAddress.Loopback, 27001);
+var Timer = new System.Timers.Timer();
+var path = "Image.png";
+
+void SS(object? source, ElapsedEventArgs? e)
+{
+    object a = new();
+    lock (a)
+    {
+        try
+        {
+            using (System.Drawing.Bitmap bitmap = new Bitmap(1920, 1080))
+            {
+                Graphics Graphics = Graphics.FromImage(bitmap);
+                Graphics.CopyFromScreen(Point.Empty, Point.Empty, new Size(1920, 1080));
+                bitmap.Save(path, ImageFormat.Png);
+            }
+
+            byte[] bytes;
+            using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                int bufferSize = 1024;
+                byte[] buffer = new byte[bufferSize];
+                int bytesRead;
+
+                while ((bytesRead = fs.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    client.Send(buffer, bytesRead, iPEndPoint);
+                }
+
+                byte end = 0x00;
+                client.Send(new byte[] { end }, 1, iPEndPoint);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
+
+
+var Any = Task.Run(() =>
+{
+    var endPoint = new IPEndPoint(IPAddress.Any, 0);
+    var request = new UdpClient(27000);
+    while (true)
+    {
+        try
+        {
+            var bytes = request.Receive(ref endPoint);
+            var str = Encoding.UTF8.GetString(bytes);
+            if (str == "Start")
+            {
+                Timer.Stop();
+                Timer.Elapsed -= SS;
+                Timer.Elapsed += SS;
+                Timer.Interval = 3000;
+                Timer.AutoReset = true;
+                Timer.Start();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+});
+Console.ReadKey();
---
+++ Listener SS/MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System.ComponentModel;
+using System.IO;
+using System.Net.Sockets;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+
+namespace Listener_SS;
+
+public partial class MainWindow : Window
+{
+    string path = "image.png";
+    UdpClient client = new(27001);
+    IPEndPoint iPEndPoint = new(IPAddress.Any, 0);
+    private string imagePath;
+    public string ImagePath { get => imagePath; set { imagePath = value; OnPropertyChanged(); } }
+    DispatcherTimer dispatcher;
+    public event PropertyChangedEventHandler? PropertyChanged;
+    public void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+    public MainWindow()
+    {
+
+        InitializeComponent();
+        DataContext = this;
+        var ByteArray = new List<byte>();
+        dispatcher = new DispatcherTimer
+        {
+            Interval = TimeSpan.FromSeconds(0.5)
+        };
+        dispatcher.Tick += ChangeImage;
+        dispatcher.Start();
+        var Any = Task.Run(() =>
+        {
+            byte end = 0x00;
+            try
+            {
+                path = $"{Guid.NewGuid()}.jpeg";
+                while (true)
+                {
+                    var bytes = client.Receive(ref iPEndPoint);
+                    if (bytes.Length == 1 && bytes[0] == end)
+                    {
+                        using var fileStream = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write);
+                        fileStream.Write(ByteArray.ToArray(), 0, ByteArray.Count);
+                        ByteArray.Clear();
+                        Application.Current.Dispatcher.Invoke(() =>
+                        {
+                            img.Source = new BitmapImage(new Uri(Path.GetFullPath(path), UriKind.RelativeOrAbsolute));
+                        });
+                    }
+                    else
+                        ByteArray.AddRange(bytes);
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        });
+
+    }
+    public BitmapImage ByteArrayToImageSource(byte[] byteArray)
+    {
+        using (var stream = new MemoryStream(byteArray))
+        {
+            var image = new BitmapImage();
+            image.BeginInit();
+            image.CacheOption = BitmapCacheOption.OnLoad;
+            image.StreamSource = stream;
+            image.EndInit();
+            image.Freeze();
+            return image;
+        }
+    }
+    private void ChangeImage(object? sender, EventArgs e)
+    {
+    }
+
+    
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            var client = new UdpClient();
+            var EndPoint = new IPEndPoint(IPAddress.Loopback, 27000);
+            var bytes = Encoding.UTF8.GetBytes("Start");
+            client.Send(bytes, bytes.Length, EndPoint);
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message);
+        }
+    }
+
+}
